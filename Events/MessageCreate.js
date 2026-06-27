b840f52f714488bf2f2a11948e53a91eeac68549
aloshTM <alosh@alosh.shop>
2026-06-26 23:02:12 -0500

    stuff

--- Events/MessageCreate.js
+++ Events/MessageCreate.js
@@ -1,4 +1,4 @@
-const { Events, EmbedBuilder, ButtonBuilder, ButtonStyle, ActionRowBuilder } = require('discord.js');
+const { Events, EmbedBuilder, ButtonBuilder, ButtonStyle, ActionRowBuilder, PermissionsBitField } = require('discord.js');
 const { HoneypotChannelId, LogChannelId, AutomaticBan } = require('../Config.json')
 
 module.exports = {
@@ -6,22 +6,25 @@ module.exports = {
     once: false,
     async execute(message) {
         try {
-            console.log(message.content)
             if (message.channelId == HoneypotChannelId ) {
                 let Embed = new EmbedBuilder().setTitle("User sent message in honeypot channel!")
                 .setDescription(`Message Author: ${message.author.displayName} (${message.author.username}),\n Message Content: ${message.content}\nEmbed Count: ${message.attachments.size}`)
                 .setThumbnail(message.author.displayAvatarURL())
                 .setTimestamp()
                 .setColor("#e67e22")
-                .setAuthor({ name: 'Pikas Discord Scam Detector', url: 'https://github.com/pl-k4' })
+                .setAuthor({ name: "Pika's Discord Scam Detector", url: "https://github.com/pl-k4" })
 
                 let LogChannel = await message.client.channels.fetch(LogChannelId)
 
                 // console.log(LogChannel)
                 if (message.member.permissions.has(PermissionsBitField.Flags.BanMembers) || message.member.permissions.has(PermissionsBitField.Flags.ModerateMembers) || message.member.permissions.has(PermissionsBitField.Flags.Administrator)) {
+                    // does the user have the Ban Members permission, Timeout Members permission, or Administrator permission? 
+                    console.log("User is a mod or admin")
+                    await message.delete() 
+                    // it will make it so the warning message gets resent upon another bot startup. im not looking for Every message in the channel. id rather kms. 
                     return // prevents mods or admins from getting banned by the bot.
                 } else {
-                    message.member.ban({deleteMessageSeconds: 60, reason: "Flagged by pikas discord scam detector"})
+                    await message.member.ban({deleteMessageSeconds: 60, reason: "Flagged by pika's discord scam detector"})
                     let unbanButton = new ButtonBuilder()
                     .setEmoji("❤️")
                     .setLabel("Unban?")
--- Events/Ready.js
+++ Events/Ready.js
@@ -1,4 +1,5 @@
 const { Events } = require('discord.js');
+const { HoneypotChannelId } = require('../Config.json')
 
 module.exports = {
 	name: Events.ClientReady,
@@ -6,6 +7,19 @@ module.exports = {
 	async execute(client) {
 		try {
 			console.log(`✅️ Logged in as ${client.user.tag}`);
+
+			let HoneypotChannel = await client.channels.fetch(HoneypotChannelId)
+
+			const warning = ":rotating_light: **Do NOT send messages in this channel, this is a honeypot channel for scam detection. If you send a message here, you will be banned from the server.** :rotating_light:";
+
+			const messages = await HoneypotChannel.messages.fetch({ limit: 1 });
+			const lastMessage = messages.first();
+
+			if (!lastMessage || lastMessage.content !== warning) {
+				HoneypotChannel.send(warning).catch((err) => {
+					console.log(err);
+				});
+			}
 		} catch (err) {
 			console.log(err);
 		}
